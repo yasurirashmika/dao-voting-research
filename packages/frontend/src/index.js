e98110b6bdba4e88c1ab31126d959f14cd395862
yasurirashmika <iyasuri.rash@gmail.com>
2025-11-03 10:41:16 +0530

    Frontend

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// 1. Import all the wagmi/rainbowkit stuff
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+// 2. Set up your config
+const config = getDefaultConfig({
+  appName: 'DAO Voting App',
+  projectId: 'YOUR_PROJECT_ID', // You get this from WalletConnect
+  chains: [mainnet, sepolia],
+});
+
+const queryClient = new QueryClient();
+
+// 3. Wrap your App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
