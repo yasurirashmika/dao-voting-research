3eb45cfe7b9a7f48b690d9b09801674077e9663d
yasurirashmika <iyasuri.rash@gmail.com>
2026-02-22 10:42:46 +0530

    cors origin resolved

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -27,7 +27,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/{*path}", cors(corsOptions));
 
 app.use(express.json({ limit: "10kb" })); // Limit payload size
 
